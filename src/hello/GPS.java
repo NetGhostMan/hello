035a87d7aa20ab69d95410588c20da73c531710e
edison <edisonhu123@sina.com>
2016-08-15 17:38:57 +0800

    GPS 还没做好，回家做

---
+++ src/hello/GPS.java
@@ -0,0 +1,21 @@
+package hello;
+
+import java.util.Scanner;
+
+public class GPS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String[] GPS = new String[100];
+		String str = in.nextLine();
+		while(!(str.equals("END"))){
+			if(){
+				
+			}
+			
+		}
+
+	}
+
+}
